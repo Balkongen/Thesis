322ce3803e090ead183ae6d387e3862b5dce0bc9
Erik Olofsson <playde5555@gmail.com>
2023-04-21 18:05:44 +0200

    Working example 1.0

--- Scripts/Dijkstras.py
+++ Scripts/Dijkstras.py
@@ -1,103 +1,190 @@
-# import heapq
+import heapq
 
-# def dijsktras(edge_list, start_node, end_node):
-#     # Step 1
+def dijsktras(edge_list, start_node, end_node):
+    # Step 1
     
-#     #dictionairy where key = a node and value is the previous node when calc dijkstras
-#     path_nodes = {}
+    #dictionairy where key = a node and value is the previous node when calc dijkstras
+    path_nodes = {}
     
     
-#     #new shit:
-#     distances = {}
-#     # for (from_node, to_node, distance) in edge_list:
-#     #     if from_node not in distances:
-#     #         distances[from_node] = float('inf')
-#     #     if to_node not in distances:
-#     #         distances[to_node] = float('inf')
+    #new shit:
+    distances = {}
+    # for (from_node, to_node, distance) in edge_list:
+    #     if from_node not in distances:
+    #         distances[from_node] = float('inf')
+    #     if to_node not in distances:
+    #         distances[to_node] = float('inf')
         
     
-#     distances = {node: float('inf') for node,_,_ in edge_list}
-#     distances[start_node] = 0
-#     print(distances)
+    distances = {node: float('inf') for node,_,_ in edge_list}
+    distances[start_node] = 0
+    print(distances)
     
-#     #print("start node:")
-#     #print(start_node)
-#     #print("end node:")
-#     #print(end_node)
-#     #print("Distances:")
-#     #print(distances)
-#     #print("End of distances")
+    #print("start node:")
+    #print(start_node)
+    #print("end node:")
+    #print(end_node)
+    #print("Distances:")
+    #print(distances)
+    #print("End of distances")
 
-#     # Step 2
-#     visited = [(0, start_node)]
-#     heapq.heapify(visited)
+    # Step 2
+    visited = [(0, start_node)]
+    heapq.heapify(visited)
     
-#     while visited: # change name
-#         # Step 3.1
-#         (curr_dist, curr_node) = heapq.heappop(visited)
+    while visited: # change name
+        # Step 3.1
+        (curr_dist, curr_node) = heapq.heappop(visited)
 
-#         print("current distance: ")
-#         print(curr_dist)
+        print("current distance: ")
+        print(curr_dist)
 
-#         print("current node:")
-#         print(curr_node)
+        print("current node:")
+        print(curr_node)
 
         
-#         #stop if visiting end_node
-#         if curr_node == end_node:
-#             break
+        #stop if visiting end_node
+        if curr_node == end_node:
+            break
 
-#         # Step 3.2
-#         for (from_node, to_node, w) in edge_list:
-#             if from_node == curr_node:
-#                 new_dist = curr_dist + w
-#                 # print("Distances", distances)
-#                 # print("No node", no_node)
-#                 # print("W", w)
-#                 if new_dist < distances[to_node]:
+        # Step 3.2
+        for (from_node, to_node, w) in edge_list:
+            if from_node == curr_node:
+                new_dist = curr_dist + w
+                # print("Distances", distances)
+                # print("No node", no_node)
+                # print("W", w)
+                if new_dist < distances[to_node]:
                     
-#                     distances[to_node] = new_dist
-#                     #här lägger vi in föregående nod
-#                     path_nodes[to_node] = from_node
-#                     heapq.heappush(visited, (new_dist, to_node))
+                    distances[to_node] = new_dist
+                    #här lägger vi in föregående nod
+                    path_nodes[to_node] = from_node
+                    heapq.heappush(visited, (new_dist, to_node))
         
         
     
-#     # Step 4
+    # Step 4
+    print("distances after")
+    print(distances)    
+    print("path nodes")
+    print(path_nodes)
 
-#     # Creates a list of the nodes between start_node and end_node
-#     path = []
-#     node = end_node
-#     while node != start_node:
-#         path.append(node)
-#         #just in case there is no path between nodes the loop stops if the previous value is None
+    # Creates a list of the nodes between start_node and end_node
+    path = []
+    node = end_node
+    while node != start_node:
+        path.append(node)
+        #just in case there is no path between nodes the loop stops if the previous value is None
     
-#         # FIXME if-satsen behövs fixas 
-#         # if path_nodes[node] != None:
-#         #     node = path_nodes[node]
-#         # else:
-#         #     break
-#         if path_nodes.get(node) != None:
-#             node = path_nodes[node]
-#         else:
-#             break
-#     path.append(start_node)
-#     path.reverse()
+        # FIXME if-satsen behövs fixas 
+        # if path_nodes[node] != None:
+        #     node = path_nodes[node]
+        # else:
+        #     break
+        if path_nodes.get(node) != None:
+            node = path_nodes[node]
+        else:
+            break
+    path.append(start_node)
+    path.reverse()
 
-#     #returnerar bara värdet. Vill returnera vägen också
-#     return path
-
-
-
-# edge_list = [(("A"), ("B"), 7), (("B"), ("A"), 7), (("A"), ("C"), 12), (("C"), ("A"), 12), (("B"), ("C"), 2), (("C"), ("B"), 2), (("B"), ("D"), 9), (("D"), ("B"), 9), (("C"), ("E"), 10), (("E"), ("C"), 10), (("D"), ("E"), 4), (("E"), ("D"), 4), (("D"), ("F"), 1), (("F"), ("D"), 1), (("E"), ("F"), 5), (("F"), ("E"), 5)]
+    #returnerar bara värdet. Vill returnera vägen också
+    return path
 
 
-# actual = dijsktras(edge_list=edge_list, start_node=("A"), end_node=("F"))
 
-# print("OUTPUT LIST")
-# print(actual)
+edge_list = [(("A"), ("B"), 7), (("B"), ("A"), 7), (("A"), ("C"), 12), (("C"), ("A"), 12), (("B"), ("C"), 2), (("C"), ("B"), 2), (("B"), ("D"), 9), (("D"), ("B"), 9), (("C"), ("E"), 10), (("E"), ("C"), 10), (("D"), ("E"), 4), (("E"), ("D"), 4), (("D"), ("F"), 1), (("F"), ("D"), 1), (("E"), ("F"), 5), (("F"), ("E"), 5)]
 
 
-arr = [1,2,3]
+actual = dijsktras(edge_list=edge_list, start_node=("A"), end_node=("D"))
 
-print(len(arr))
+print("OUTPUT LIST")
+print(actual)
+
+
+# arr = [1,2,3]
+
+# print(len(arr))
+
+
+nodes = [] # [(x-coordinate, y-coordinate), ...]
+
+edges = []  # [(from_node, to_node, distance), ...]
+
+
+edges.append(((3, 5), (5, 5), 5))
+edges.append(((5, 5), (5, 7), 5))
+edges.append(((5, 7), (2, 2), 5))
+edges.append(((2, 2), (1, 1), 5))
+edges.append(((1, 1), (7, 5), 5))
+# node = (x-coordinate, y-coordinate)
+
+node_to_energy = {} # key = node (x-coordinate, y-coordinate). Value = residual energy of node
+
+node_to_energy = {}
+
+node_to_energy[(3, 5)] = 1
+node_to_energy[(5, 5)] = 1
+node_to_energy[(5, 7)] = 1
+node_to_energy[(2, 2)] = 1
+node_to_energy[(1, 1)] = 1
+node_to_energy[(7, 5)] = 1
+
+
+path = [(3, 5), (5, 5), (5, 7), (2, 2), (1, 1), (7, 5)]
+
+A = 1.0e-9
+B = 5.0e-11
+m = 4
+
+def transmission_eq3(distance, k):
+    return (A + B * distance**m) * k
+
+# Equation 4, Energy reseption cost
+def reception_eq4(k):
+    return A * k
+
+
+
+def send_data_and_compute_new_energy(path, k):
+    
+    # tranmission cost on start_node
+
+    for index, node in enumerate(path):
+        
+        if index == len(path) - 1:
+            return
+
+        next_node = path[index + 1]
+        tmp_distance = None
+        current_energy = node_to_energy[node]
+        
+        for start_node, end_node, distance in edges:
+            if start_node == node and end_node == next_node:
+                tmp_distance = distance
+
+        # To node
+        # print("current node, transmission cost and reception cost")
+        # print(node)
+        # print(transmission_eq3(tmp_distance, k))
+        # print(reception_eq4(k))
+        # print("end")
+
+        if index == 0:
+            # Do Only transmission on first node
+            node_to_energy[node] = current_energy - transmission_eq3(tmp_distance, k)
+            continue
+    
+        
+        node_to_energy[node] = current_energy - transmission_eq3(tmp_distance, k) - reception_eq4(k)
+
+for x in node_to_energy:
+    print(x, node_to_energy[x])
+
+print("----------------")
+
+for x in range(0, 1000):
+    send_data_and_compute_new_energy(path=path, k=100)
+
+for x in node_to_energy:
+    print(x, node_to_energy[x])
--- Scripts/script.py
+++ Scripts/script.py
@@ -6,7 +6,7 @@ import heapq
 ENERGY = 1
 COLUMNS = 25
 ROWS = 25
-NUMBER_OF_NODES = 10
+NUMBER_OF_NODES = 100
 
 RADIO_DIST = 7
 
@@ -18,6 +18,8 @@ edges = [] # [(from_node, to_node, distance), ...]
 # node = (x-coordinate, y-coordinate)
 
 node_to_energy = {} # key = node (x-coordinate, y-coordinate). Value = residual energy of node
+
+node_to_maximum_path = {} # key = node (x-coordinate, y-coordinate). Value = maximum path to sink node
 
 
 def create_network(rows, columns, num_of_nodes, nodes, node_energy):
@@ -66,7 +68,9 @@ m = 4
 
 # Equation 3, Energy transmission cost
 def transmission_eq3(distance, k):
+ 
     return (A + B * distance**m) * k
+    # return 0
 
 # Equation 4, Energy reseption cost
 def reception_eq4(k):
@@ -85,24 +89,26 @@ def lifetime_membership_eq6(residual_energy, distance, k):
     
     current_energy = residual_energy - transmission_eq3(distance, k)
 
-    if ALPHA * SIGMA < current_energy and current_energy <= SIGMA:
-        #lollolo
+    if (ALPHA * SIGMA) < current_energy and current_energy <= SIGMA:
         return 1 - ((1 - GAMMA) / (1 - ALPHA)) * (1 - current_energy / SIGMA)
         
-    elif transmission_eq3(distance, k) < current_energy and current_energy <= ALPHA * SIGMA:
+    elif transmission_eq3(distance, k) < current_energy and current_energy <= (ALPHA * SIGMA):
         condition_1 = (GAMMA) / (ALPHA * (SIGMA - transmission_eq3(distance, k))) 
         condition_2 = current_energy - transmission_eq3(distance, k)
         return condition_1 * condition_2
 
     elif current_energy <= transmission_eq3(distance, k):
         return 0
-
-
+    else:
+        return 1 # Sink node
+    
+    
 THETA = [0.2, 0.8]
 
 # Equation 7, fuzzy membership minimun delay
-def minimum_delay_eq7(x):
-    return x
+def minimum_delay_eq7(node):
+    
+    return 1 + (((THETA[0] - 1) * node_to_maximum_path[node]) / maximum_path_distance) # THETA can change based on test runs
 
 
 # Equation 10, mulitobjective membership function
@@ -113,8 +119,7 @@ def multi_objective_membership_eq10(umd, ulf):
 # Equation 11, returns new value of weight of edge
 def weight_assign_eq11(edge, package_size):
     residual_energy = node_to_energy[edge[0]]
-    #DON'T FORGET X FFS
-    return 1 - multi_objective_membership_eq10(lifetime_membership_eq6(residual_energy, edge[2], package_size), minimum_delay_eq7(1000))
+    return 1 - multi_objective_membership_eq10(lifetime_membership_eq6(residual_energy, edge[2], package_size), minimum_delay_eq7(edge[0]))
 
 # Must be of existing node
 # TODO TEST
@@ -123,19 +128,22 @@ def weight_assign_eq11(edge, package_size):
 # FIXME GÖR OM ELLER TA BORT
 def create_routes(routes):
 
-    for _ in range(0, 10_000):
+    for _ in range(0, 1000):
         index = random.randint(0, len(nodes) - 1)
-        package_size = random.randint(16, 64) # Ask supervisor for correct reasonable size
+        package_size = 1000 # Ask supervisor for correct reasonable size
         routes.append((nodes[index], package_size))
         
 
 def dijsktras(edge_list, start_node, end_node):
     # Step 1
-    
+    # print("start and end node")
+    # print(start_node, end_node)
     #dictionairy where key = a node and value is the previous node when calc dijkstras
     path_nodes = {}
+    path_nodes[end_node] = None
+    #add source node
+    #path_nodes[start_node] = None
     distances = {node: float('inf') for node,_,_ in edge_list}
-    
 
     distances[start_node] = 0
     
@@ -164,29 +172,25 @@ def dijsktras(edge_list, start_node, end_node):
                     #här lägger vi in föregående nod
                     path_nodes[to_node] = from_node
                     heapq.heappush(visited, (new_dist, to_node))
-        
-        
-    
+
     # Step 4
 
     # Creates a list of the nodes between start_node and end_node
     path = []
+    
     node = end_node
     while node != start_node:
         path.append(node)
         #just in case there is no path between nodes the loop stops if the previous value is None
-    
-        # FIXME if-satsen behövs fixas 
-        # if path_nodes[node] != None:
-        #     node = path_nodes[node]
-        # else:
-        #     break
-        if path_nodes.get(node) != None:
+
+            
+        if path_nodes[node] is not None:
             node = path_nodes[node]
         else:
             break
     path.append(start_node)
     path.reverse()
+
 
     #returnerar bara värdet. Vill returnera vägen också
     return path
@@ -194,18 +198,11 @@ def dijsktras(edge_list, start_node, end_node):
 
 def send_data_and_compute_new_energy(path, k):
     
-    # tranmission cost on start_node
-    
-    print(path)
-    
     for index, node in enumerate(path):
-        if index == 0:
-            # Do Only transmission
-            continue
+        
         if index == len(path) - 1:
             return
-        #sending data
-        
+
         next_node = path[index + 1]
         tmp_distance = None
         current_energy = node_to_energy[node]
@@ -214,57 +211,110 @@ def send_data_and_compute_new_energy(path, k):
             if start_node == node and end_node == next_node:
                 tmp_distance = distance
 
-        # To node
-        print("current node, transmission cost and reception cost")
-        print(node)
-        print(transmission_eq3(tmp_distance, k))
-        print(reception_eq4(k))
-        print("end")
+
+        if index == 0:
+            # Do Only transmission on first node
+            node_to_energy[node] = current_energy - transmission_eq3(tmp_distance, k)
+            continue
+        
+        #sending data
         node_to_energy[node] = current_energy - transmission_eq3(tmp_distance, k) - reception_eq4(k)
 
 
-def main():
+def disco_disk(edge_list, start_node, end_node):
+    
+    distances = {node: float('inf') for node,_,_ in edge_list}
 
+    distances[start_node] = 0
+    
+    # Step 2
+    visited = [(0, start_node)]
+    heapq.heapify(visited)
+    
+    while visited: # change name
+        # Step 3.1
+        (curr_dist, curr_node) = heapq.heappop(visited)
+        
+        #stop if visiting end_node
+        if curr_node == end_node:
+            break
+
+        # Step 3.2
+        for (from_node, to_node, w) in edge_list:
+            if from_node == curr_node:
+                new_dist = curr_dist + w
+                # print("Distances", distances)
+                # print("No node", no_node)
+                # print("W", w)
+                if new_dist < distances[to_node]:
+                    
+                    distances[to_node] = new_dist
+                    #här lägger vi in föregående nod
+                    heapq.heappush(visited, (new_dist, to_node))
+
+
+    return distances[end_node]
+
+
+
+def driver():
+    for node in nodes:
+        node_to_maximum_path[node] = disco_disk(edge_list=edges, start_node=node, end_node=(0, 0))
+          
+    
+    return max(node_to_maximum_path.values())
+
+
+
+maximum_path_distance = driver()
+
+
+def main():
     #dict with edge - weight in this context
     edge_weight = {}
 
     # Initialize routing requests
     routing_request = []
-    # create_routes(routing_request)
-    routing_request.append(nodes[1])
-
+    create_routes(routing_request)
+    
     lifetime_count = 0
-
+    
     # for all routing requests
     for request in routing_request:
         # for each edge in network
         for i, edge in enumerate(edges):
+            
             #assign weight to edges and add to dict
             edge_weight[edge] = weight_assign_eq11(edge, request[1])
 
-        #find shortest path based on new weights
-        #TODO ska request vara indexerat med 0??
-        ###BORTA
+       
 
-        print("start node:")
-        print(request)
-        minimum_weight_path = dijsktras(edge_weight, request, (0, 0))
+        # print("start node:")
+        # print(request)
+        minimum_weight_path = dijsktras(edge_weight, request[0], (0, 0))
         
+        # print("start node, end node: ", request)
+        # print("MiNI", minimum_weight_path)
 
-        send_data_and_compute_new_energy(minimum_weight_path, 1000)
+        if len(minimum_weight_path) == 1:
+            
+            continue
+        else:
+
+            send_data_and_compute_new_energy(minimum_weight_path, 1000)
         
-        #check_energy_levels
-            #if any node energy = 0
-            #return
-        lifetime_count = lifetime_count = 1
+    
+        for node in node_to_energy:
+            if node_to_energy[node] <= 0:
+                return lifetime_count
+            
+        lifetime_count = lifetime_count + 1
     
     return lifetime_count
 
 
-
-
 if __name__ == '__main__':
-    main()
+    print("lifetime", main())
     print("NODES")
     for i, node in enumerate(node_to_energy):
         print(node, node_to_energy[node])
